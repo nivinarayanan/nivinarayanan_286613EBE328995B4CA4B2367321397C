adfb6354ae7b3685341df8a47e9a981b9b72a38d
nivinarayanan <145776077+nivinarayanan@users.noreply.github.com>
2023-09-27 21:39:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-# Define the base class Player
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
-
-# Define the derived class Batsman
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
-
-# Define the derived class Bowler
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
-
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      # self.__account_balance = self.__account_balance+amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,
+                                                     self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__account_balance = self.__account_balance - amount
+      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}".format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
+# Create an instance of the BankAccount class
+account = BankAccount(account_number="987654321",
+                      account_holder_name="Kalpana",
+                      initial_balance=5000.0)
+# Test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
